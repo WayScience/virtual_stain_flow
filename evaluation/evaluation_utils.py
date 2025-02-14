744efcc90de6e2844a48e889f05291f19d0bc412
Weishan Li <wli51@jh.edu>
2025-02-14 14:30:36 -0700

    Added evaluation files

---
+++ evaluation/evaluation_utils.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+from typing import List, Callable
+
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader
+
+def evaluate_metrics(
+    _model: torch.nn.Module,
+    _dataset: torch.utils.data.Dataset,
+    _metrics: List[Callable|torch.nn.Module],
+    _device:str='cpu'
+):    
+    metrics = defaultdict(list)
+    _model.to(_device)
+    _model.eval()
+
+    data_loader = DataLoader(_dataset, batch_size=1, shuffle=False)
+
+    with torch.no_grad():    
+        for input, target in data_loader:
+            input = input.to(_device)
+            target = target.to(_device)
+            output = _model(input)            
+            for _metric in _metrics:
+                metrics[_metric.__class__.__name__].append(_metric(output, target).item())
+    
+    return pd.DataFrame(metrics)
