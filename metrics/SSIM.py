cde174c3ba54cf442aa29527c563903345ccb8cf
Weishan Li <wli51@jh.edu>
2025-02-16 19:22:51 -0700

    Removed redundant metric name property and added documentation

--- metrics/SSIM.py
+++ metrics/SSIM.py
@@ -1,4 +1,5 @@
 import torch
+
 from .AbstractMetrics import AbstractMetrics
 
 """
@@ -8,13 +9,31 @@ class SSIM(AbstractMetrics):
     """Computes and tracks the Structural Similarity Index Measure (SSIM)."""
 
     def __init__(self, _metric_name: str, _max_pixel_value: int = 1):
+        """
+        Initializes the SSIM metric.
+
+        :param _metric_name: The name of the metric.
+        :type _metric_name: str
+        :param _max_pixel_value: The maximum possible pixel value of the images, by default 1.
+        :type _max_pixel_value: int, optional
+        """
 
         super(SSIM, self).__init__(_metric_name)
         
         self.__max_pixel_value = _max_pixel_value
 
     def forward(self, _generated_outputs: torch.Tensor, _targets: torch.Tensor):
+        """
+        Computes the Structural Similarity Index Measure (SSIM) between the generated outputs and the target images.
 
+        :param _generated_outputs: The tensor containing the generated output images.
+        :type _generated_outputs: torch.Tensor
+        :param _targets: The tensor containing the target images.
+        :type _targets: torch.Tensor
+        :return: The computed SSIM value.
+        :rtype: torch.Tensor
+        """
+        
         mu1 = _generated_outputs.mean(dim=[2, 3], keepdim=True)
         mu2 = _targets.mean(dim=[2, 3], keepdim=True)
 
@@ -32,7 +51,3 @@ class SSIM(AbstractMetrics):
         )
 
         return ssim_value.mean()
-
-    @property
-    def metric_name(self):
-        return self.__metric_name
