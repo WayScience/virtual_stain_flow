74b7b02f6614c448196587f3f48ca3772d98714c
Weishan Li <wli51@jh.edu>
2025-02-18 10:12:45 -0700

    Added wrapper class for torch modules that accumulates metric values across batches

---
+++ metrics/MetricsWrapper.py
@@ -0,0 +1,17 @@
+import torch
+
+from .AbstractMetrics import AbstractMetrics
+
+class MetricsWrapper(AbstractMetrics):
+    """Metrics wrapper class that wraps a pytorch module
+    and calls it forward pass function to accumulate the metric 
+    values across batches
+    """
+
+    def __init__(self, _metric_name: str, module: torch.nn.Module):
+        
+        super(MetricsWrapper, self).__init__(_metric_name)
+        self._module = module
+
+    def forward(self,_generated_outputs: torch.Tensor, _targets: torch.Tensor):
+        return self._module(_generated_outputs, _targets).mean()
