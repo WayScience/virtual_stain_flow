071aa8717bddf6c3424161bdb21ac27d9eb1a218
Weishan Li <wli51@jh.edu>
2025-02-26 11:28:44 -0700

    Modified callback to allow the plot frequency during training to be tuned

--- callbacks/IntermediatePlot.py
+++ callbacks/IntermediatePlot.py
@@ -20,6 +20,7 @@ class IntermediatePatchPlot(AbstractCallback):
                  dataset: PatchDataset, 
                  plot_n_patches: int=5,
                  plot_metrics: List[nn.Module]=None,
+                 every_n_epochs: int=5,
                  **kwargs):
         """
         Initialize the IntermediatePlot callback.
@@ -36,6 +37,8 @@ class IntermediatePatchPlot(AbstractCallback):
         :type plot_metrics: List[nn.Module], optional
         :param kwargs: Additional keyword arguments to be passed to plot_patches.
         :type kwargs: dict
+        :param every_n_epochs: How frequent should intermediate plots should be plotted, defaults to 5
+        :type every_n_epochs: int
         :raises TypeError: If the dataset is not an instance of PatchDataset.
         """
         super().__init__(name)
@@ -47,15 +50,30 @@ class IntermediatePatchPlot(AbstractCallback):
         # Additional kwargs passed to plot_patches
         self.plot_n_patches = plot_n_patches
         self.plot_metrics = plot_metrics
+        self.every_n_epochs = every_n_epochs
         self.plot_kwargs = kwargs
 
     def on_epoch_end(self):
         """
-        Called at the end of each epoch.
-
-        Plot dataset with model predictions on n random images from dataset at the end of each epoch.
+        Called at the end of each epoch to plot predictions if the epoch is a multiple of `every_n_epochs`.
         """
+        if (self.trainer.epoch + 1) % self.every_n_epochs == 0 or self.trainer.epoch + 1 == self.trainer.total_epochs:
+            self._plot()
+    
+    def on_train_end(self):
+        """
+        Called at the end of training. Plots if not already done in the last epoch.
+        """
+        if (self.trainer.epoch + 1) % self.every_n_epochs != 0:
+            self._plot()
 
+    def _plot(self):
+        """
+        Helper method to generate and save plots.
+        Plot dataset with model predictions on n random images from dataset at the end of each epoch.
+        Called by the on_epoch_end and on_train_end methods
+        """
+        
         original_device = next(self.trainer.model.parameters()).device
 
         plot_patches(
