Fixed bug of higher order derivative not being able to be computed due to not retaining the graph

losses/GradientPenaltyLoss.py
@@ -38,7 +38,7 @@ class GradientPenaltyLoss(AbstractLoss):
             inputs=interpolated,
             grad_outputs=torch.ones_like(prob_interpolated),
             create_graph=True,
-            retain_graph=False,
+            retain_graph=True,
         )[0]
 
         gradients = gradients.view(batch_size, -1)