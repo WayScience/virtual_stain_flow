1b5d7ec0af9f3bdaf797345f90b8913fa46cdb6d
Weishan Li <wli51@jh.edu>
2025-03-03 00:02:40 -0700

    Modified evlauation_per_image_metric function so metrics cna be computed on a subset of the target prediciton pairs.

--- evaluation/evaluation_utils.py
+++ evaluation/evaluation_utils.py
@@ -1,37 +1,15 @@
-from collections import defaultdict
-from typing import List, Dict, Callable, Union
+from typing import List, Optional
 
 import pandas as pd
 import torch
 from torch.nn import Module
 from torch.utils.data import DataLoader
 
-def evaluate_metrics(
-    _model: torch.nn.Module,
-    _dataset: torch.utils.data.Dataset,
-    _metrics: List[Union[Callable, torch.nn.Module]],
-    _device:str='cpu'
-):    
-    metrics = defaultdict(list)
-    _model.to(_device)
-    _model.eval()
-
-    data_loader = DataLoader(_dataset, batch_size=1, shuffle=False)
-
-    with torch.no_grad():    
-        for input, target in data_loader:
-            input = input.to(_device)
-            target = target.to(_device)
-            output = _model(input)            
-            for _metric in _metrics:
-                metrics[_metric.__class__.__name__].append(_metric(output, target).item())
-    
-    return pd.DataFrame(metrics)
-
 def evaluate_per_image_metric(
     predictions: torch.Tensor,
     targets: torch.Tensor,
-    metrics: List[Module]
+    metrics: List[Module],
+    indices: Optional[List[int]] = None
 ) -> pd.DataFrame:
     """
     Computes a set of metrics on a per-image basis and returns the results as a pandas DataFrame.
@@ -42,6 +20,8 @@ def evaluate_per_image_metric(
     :type targets: torch.Tensor
     :param metrics: List of metric functions to evaluate.
     :type metrics: List[torch.nn.Module]
+    :param indices: Optional list of indices to subset the dataset before inference. If None, all images are evaluated.
+    :type indices: Optional[List[int]], optional
 
     :return: A DataFrame where each row corresponds to an image and each column corresponds to a metric.
     :rtype: pd.DataFrame
@@ -51,7 +31,10 @@ def evaluate_per_image_metric(
 
     results = []
 
-    for i in range(predictions.shape[0]):  # Iterate over images
+    if indices is None:
+        indices = range(predictions.shape[0])
+
+    for i in indices:  # Iterate over images/subset
         pred, target = predictions[i].unsqueeze(0), targets[i].unsqueeze(0)  # Keep batch dimension
         metric_scores = {metric.__class__.__name__: metric.forward(target, pred).item() for metric in metrics}
         results.append(metric_scores)
