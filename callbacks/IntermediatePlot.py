f909e342a805b0b6b793f07c47ea44b99216c62d
Weishan Li <wli51@jh.edu>
2025-03-03 00:49:09 -0700

    Updated IntermedaitePlot callback class for compatibility with new plotting function

--- callbacks/IntermediatePlot.py
+++ callbacks/IntermediatePlot.py
@@ -1,11 +1,13 @@
 from typing import List, Union
+import random
 
 import torch
 import torch.nn as nn
+from torch.utils.data import Dataset
 
 from .AbstractCallback import AbstractCallback
 from ..datasets.PatchDataset import PatchDataset
-
+from ..evaluation.visualization_utils import plot_predictions_grid_from_model
 from ..evaluation.visualization_utils import plot_patches
 
 class IntermediatePatchPlot(AbstractCallback):
@@ -17,28 +19,38 @@ class IntermediatePatchPlot(AbstractCallback):
     def __init__(self,                
                  name: str, 
                  path: str, 
-                 dataset: PatchDataset, 
+                 dataset: Union[Dataset, PatchDataset], 
                  plot_n_patches: int=5,
+                 indices: Union[List[int], None]=None,
                  plot_metrics: List[nn.Module]=None,
                  every_n_epochs: int=5,
+                 random_seed: int=42,
                  **kwargs):
         """
-        Initialize the IntermediatePlot callback.
+        Initialize the IntermediatePlot callback. 
+        This callback, when passed into the trainer, will plot the model predictions on a subset of the provided dataset at the end of each epoch.
 
         :param name: Name of the callback.
         :type name: str
         :param path: Path to save the model weights.
         :type path: str
         :param dataset: Dataset to be used for plotting intermediate results.
-        :type dataset: PatchDataset
-        :param plot_n_patches: Number of patches to plot, defaults to 5.
+        :type dataset: Union[Dataset, PatchDataset]
+        :param plot_n_patches: Number of patches to randomly select and plot, defaults to 5.
+        The exact patches/images being plotted may vary due to a difference in seed or dataset size. 
+        To ensure best reproducibility and consistency, please use a fixed dataset and indices argument instead.
         :type plot_n_patches: int, optional
+        :param indices: Optional list of specific indices to subset the dataset before inference.
+        Overrides the plot_n_patches and random_seed arguments and uses the indices list to subset. 
+        :type indices: Union[List[int], None]
         :param plot_metrics: List of metrics to compute and display in plot title, defaults to None.
         :type plot_metrics: List[nn.Module], optional
         :param kwargs: Additional keyword arguments to be passed to plot_patches.
         :type kwargs: dict
         :param every_n_epochs: How frequent should intermediate plots should be plotted, defaults to 5
         :type every_n_epochs: int
+        :param random_seed: Random seed for reproducibility for random patch/image selection, defaults to 42.
+        :type random_seed: int
         :raises TypeError: If the dataset is not an instance of PatchDataset.
         """
         super().__init__(name)
@@ -48,10 +60,21 @@ class IntermediatePatchPlot(AbstractCallback):
         self._dataset = dataset
 
         # Additional kwargs passed to plot_patches
-        self.plot_n_patches = plot_n_patches
         self.plot_metrics = plot_metrics
         self.every_n_epochs = every_n_epochs
         self.plot_kwargs = kwargs
+        
+        if indices is not None:
+            # Check if indices are within bounds
+            for i in indices:
+                if i >= len(self._dataset):
+                    raise ValueError(f"Index {i} out of bounds for dataset of size {len(self._dataset)}")
+            self._dataset_subset_indices = indices
+        else:
+            # Generate random indices to subset given seed and plot_n_patches
+            plot_n_patches = min(plot_n_patches, len(self._dataset))
+            random.seed(random_seed)
+            self._dataset_subset_indices = random.sample(range(len(self._dataset)), plot_n_patches)
 
     def on_epoch_end(self):
         """
@@ -76,12 +99,13 @@ class IntermediatePatchPlot(AbstractCallback):
         
         original_device = next(self.trainer.model.parameters()).device
 
-        plot_patches(
-            _dataset = self._dataset,
-            _n_patches = self.plot_n_patches,
-            _model = self.trainer.model,
-            _metrics = self.plot_metrics,
-            save_path = f"{self._path}/epoch_{self.trainer.epoch}.png",
+        plot_predictions_grid_from_model(
+            model=self.trainer.model,
+            dataset=self._dataset,
+            indices=self._dataset_subset_indices,
+            metrics=self.plot_metrics,
+            save_path=f"{self._path}/epoch_{self.trainer.epoch}.png",
             device=original_device,
+            show=False,
             **self.plot_kwargs
         )
