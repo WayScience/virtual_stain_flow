885663b3114017f8bcafaa7b0eb44103a1133cb8
Weishan Li <wli51@jh.edu>
2025-06-19 15:11:15 -0600

    Added new abstract trainer and trainer implementations (minimal modifcations from the old AbstractTrainer.py and Trainer.py) to support the new logging class.

---
+++ trainers/__init__.py
@@ -0,0 +1,12 @@
+from .AbstractTrainer import AbstractTrainer
+from .AbstractLoggingTrainer import AbstractLoggingTrainer
+from .Trainer import Trainer
+from .TrainerV2 import TrainerV2
+from .WGANTrainer import WGANTrainer
+
+__all__ = [
+    'AbstractTrainer',
+    'AbstractLoggingTrainer',
+    'Trainer',
+    'WGANTrainer',
+]
