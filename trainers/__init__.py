ecf577a466c9070e2f5cc1577b2b2bfb67c25a09
Weishan Li <wli51@jh.edu>
2025-07-14 18:13:16 -0600

    Moved AbstractLoggingTrainer.py and TrainerV2.py into trainers.logging_trainers module. Update __init__.py of the trainers module to reflect the relocation.

--- trainers/__init__.py
+++ trainers/__init__.py
@@ -1,12 +1,9 @@
 from .AbstractTrainer import AbstractTrainer
-from .AbstractLoggingTrainer import AbstractLoggingTrainer
 from .Trainer import Trainer
-from .TrainerV2 import TrainerV2
 from .WGANTrainer import WGANTrainer
 
 __all__ = [
     'AbstractTrainer',
-    'AbstractLoggingTrainer',
     'Trainer',
     'WGANTrainer',
 ]
