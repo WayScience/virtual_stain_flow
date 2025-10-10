ea363a773168527836e113463aba032e30dd9227
Weishan Li <wli51@jh.edu>
2025-10-10 11:20:35 -0600

    Add utility functions for loss handling in loss_utils.py

---
+++ src/virtual_stain_flow/losses/loss_utils.py
@@ -0,0 +1,50 @@
+"""
+loss_utils.py
+
+Utility functions for loss handling.
+"""
+
+from typing import Union, Dict
+
+import torch
+
+from .AbstractLoss import AbstractLoss
+
+
+def _get_loss_name(
+        loss_fn: Union[torch.nn.Module, AbstractLoss]
+) -> str:
+    """
+    Helper method to get the name of the loss function.
+    """
+    if isinstance(loss_fn, AbstractLoss) and hasattr(loss_fn, "metric_name"):
+        return loss_fn.metric_name
+    elif isinstance(loss_fn, torch.nn.Module):
+        return type(loss_fn).__name__
+    else:
+        raise TypeError(
+            "Expected loss_fn to be either a torch.nn.Module or "
+            "an AbstractLoss instance."
+            f"Got {type(loss_fn)} instead."
+        )
+    
+def _scalar_from_device(
+    value: float, 
+    device: torch.device,
+    dtype: torch.dtype = torch.float32,
+):
+    """
+    Helper method to convert a scalar value to a tensor in the specified device.
+    """
+    return torch.tensor(value, device=device, dtype=dtype)
+
+def _scalar_from_ctx(
+    value: float,
+    ctx: Dict[str, torch.Tensor]
+):
+    """
+    Helper method to convert a scalar value on the same device and 
+        of the same dtype as a given context object (dictionary of tensors)
+    """
+    t = next(iter(ctx.values()))
+    return _scalar_from_device(value, t.device, t.dtype)
